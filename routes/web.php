d07d8283d59c613e11bde6a7824a50865272d6c5
mahymooo <moha.isme222@gmail.com>
2024-04-25 00:21:59 +0200

    add new feature

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('mahy/{id}',function($id){
+    return "welcome to my website" .$id;
+});
